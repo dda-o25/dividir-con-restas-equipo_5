564d267c33a2168965f337b3d5c92c868f0b9e9b
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-10-06 20:24:33 -0600

    prueba1

--- dividir_con_restas.py
+++ dividir_con_restas.py
@@ -1,15 +1,24 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Larisa Carolina Alvarez Gonzalez
+
+Diseña un programa para calcular el cociente y residuo de una división mediante restas sucesivas
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+dividendo = int(input("Introduzca el dividendo: "))
+divisor = int(input("Introduzca el divisor: "))
+
 
 # Proceso
+contador = 0
+residuo = dividendo
+while residuo >= divisor:
+    residuo -= divisor
+    contador += 1
+
+cociente = contador
 
 
 # Salidas
-print(salida)
+print(f"El cociente es {cociente}")
+print(f"El residuo es {residuo}")
